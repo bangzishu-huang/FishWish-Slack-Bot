0274a162d3318c973fe905b3e5bc7f4c1d3e5147
bangzishu-huang <bangzishu@gmail.com>
2026-07-05 22:17:40 -0400

    added some functions and needed packages for slack bot

---
+++ index.js
@@ -0,0 +1,71 @@
+const axios = require("axios");
+
+require("dotenv").config();
+
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true
+});
+
+app.command("/fishwish-ping", async ({ command, ack, respond }) => {
+  const start = Date.now();
+  await ack();
+  const latency = Date.now() - start;
+  await respond({ text: `Pong!\nLatency: ${latency}ms` });
+});
+
+(async () => {
+  await app.start();
+  console.log("bot is running!");
+})();
+
+app.command("/fishwish-catfact", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://catfact.ninja/fact");
+    await respond({ text: `Cat Fact:\n${response.data.fact}` });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a cat fact." });
+  }
+});
+
+
+app.command("/fishwish-catgif", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://cataas.com/cat/gif",{
+        headers: {Accept: "application/json"}
+    });
+    
+    const catData = response.data;
+    const catGifUrl = catData.url
+
+    await respond({
+      blocks: [
+        {
+          type: "image",
+          image_url: catGifUrl,
+          alt_text: "A random cat gif"
+        }
+      ]
+    });
+
+  } catch (err) {
+    console.log("Full error:", err.message);
+    console.log("Error response data:", err.response?.data);
+    console.log("Error response status:", err.response?.status);
+    await respond({ text: "Failed to fetch a cat gif." });
+  }
+});
+
+
+// format
+
+// app.command("/command-name", async ({ ack, respond }) => {
+//   // your code here
+// });
