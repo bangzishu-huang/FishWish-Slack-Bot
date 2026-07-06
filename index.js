cb623bac26f502c6aa0f346c074e43e197e7defe
bangzishu-huang <bangzishu@gmail.com>
2026-07-06 17:14:38 -0400

    tweaking brightness filter

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ app.command("/fishwish-greet", async({ ack, respond }) => {
 app.command("/fishwish-catfilter", async({ ack, respond }) => {
   await ack()
 
-  const brightness = Math.floor(Math.random() * 7) + 1;
+  const brightness = Math.floor(Math.random() * 5) + 1;
   const lightness = Math.floor(Math.random() * 11) - 5;
   const saturation = Math.floor(Math.random() * 11);
   const hue = Math.floor(Math.random() * 16) - 15;
